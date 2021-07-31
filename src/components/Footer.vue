e3e6f5671693df8134fd2a2c0a9a253f835a87d7
Luis Rodriguez <rodriguezlf4@gmail.com>
2021-07-30 22:57:25 -0700

    fix: make resume a download

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
             <a
               class="has-text-weight-bold is-size-4 has-text-danger"
               href="./assets/LuisRodriguezResume.pdf"
-              target="_blank"
+              download
               >Download Resume</a
             >
           </li>
